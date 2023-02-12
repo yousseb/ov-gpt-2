260b5252d7099caa23099f1eb5125ab7e128714b
Youssef A. Abukwaik <youssef.adnan@gmail.com>
2023-02-11 19:35:11 -0800

    Fix CI

--- main.py
+++ main.py
@@ -9,14 +9,33 @@ from argparse import ArgumentParser, SUPPRESS
 log.basicConfig(format='[ %(levelname)s ] %(message)s', level=log.INFO, stream=sys.stdout)
 
 
+def build_argparser():
+    parser = ArgumentParser(add_help=False)
+    args = parser.add_argument_group('Options')
+    args.add_argument('-h', '--help', action='help', default=SUPPRESS, help='Show this help message and exit.')
+    args.add_argument("-i", "--input", help="Optional. Input prompt", required=False, type=str, action='append')
+    return parser
+
+
 if __name__ == '__main__':
+
+    parser = build_argparser().parse_args()
 
     ModelArtifactsDownloader().check_files('model')
     config = OVGPT2Config()
     gpt2 = OVGPT2(config)
 
-    while True:
-        x = input('Q:')
-        response = gpt2.infer(x)
+    if parser.input:
+        def prompts():
+            for prompt in parser.input:
+                log.info("Q: {}".format(prompt))
+                yield prompt
+    else:
+        def prompts():
+            while True:
+                yield input('Q:')
+
+    for prompt in prompts():
+        response = gpt2.infer(prompt)
         print(f'GPT2: {response}\n')
         print("-" * 70)
